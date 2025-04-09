4fe7e901ca8525d7b2186fc1498a3b9f4410f241
fassabilf <fassabilf@gmail.com>
2025-04-09 10:55:33 +0700

    chore: initialize project for log-service

---
+++ src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/log/LogServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b13.hiringgo.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/b13/hiringgo/log/LogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b13.hiringgo.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
