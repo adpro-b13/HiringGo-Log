98c63094ca1cb8a0a745eb64cae2bf513e8ddb2c
joseph <yosiajosephc@gmail.com>
2025-04-11 17:29:39 +0700

    comment out unnecessary test

--- src/test/java/id/ac/ui/cs/advprog/b13/hiringgo/log/LogServiceApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/b13/hiringgo/log/LogServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package id.ac.ui.cs.advprog.b13.hiringgo.log;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LogServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package id.ac.ui.cs.advprog.b13.hiringgo.log;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LogServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
