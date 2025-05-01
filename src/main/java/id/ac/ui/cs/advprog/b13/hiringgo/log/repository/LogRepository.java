2bb4ba50a0cca06150c2e821b101d91dba478061
joseph <yosiajosephc@gmail.com>
2025-05-01 18:02:39 +0700

    annotate repository

--- src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/log/repository/LogRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/b13/hiringgo/log/repository/LogRepository.java
@@ -3,6 +3,9 @@ package id.ac.ui.cs.advprog.b13.hiringgo.log.repository;
 import id.ac.ui.cs.advprog.b13.hiringgo.log.model.Log;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface LogRepository {
     Log save(Log log);
     Log findById(Long id);
