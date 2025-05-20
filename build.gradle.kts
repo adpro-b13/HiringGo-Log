b818828d8ed847622b44c70c50c09739780bf705
joseph <yosiajosephc@gmail.com>
2025-05-20 13:04:04 +0700

    [REFACTOR] maintainability: Group dependencies by their destination

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "6.0.1.5171"
@@ -34,22 +35,22 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")  // if you need REST API capabilities
     implementation("org.springframework.boot:spring-boot-starter-logging") // for logging
     implementation("org.postgresql:postgresql:42.5.0")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    testImplementation("com.h2database:h2") // Added H2 for testing
     implementation("jakarta.validation:jakarta.validation-api:3.0.2")
-
     implementation("com.h2database:h2")
+
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2") // Added H2 for testing
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
